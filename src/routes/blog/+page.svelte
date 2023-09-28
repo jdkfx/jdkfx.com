79f80f3fa425615ffb0f714629c56fcd1f4c9cbd
jdkfx <amateurranch@gmail.com>
2023-09-28 23:05:56 +0900

    :sparkles: ブログ機能の追加

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Blog</title>
+	<meta name="description" content="Blog Posts" />
+</svelte:head>
+
+<script>
+	export let data;
+</script>
+
+<div class="text-column">
+	<h1>Blog</h1>
+
+	<ul>
+		{#each data.summaries as { slug, title }}
+			<li><a href="/blog/{slug}">{title}</a></li>
+		{/each}
+	</ul>
+</div>
