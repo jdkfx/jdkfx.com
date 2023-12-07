dc9b41680da5cbc5cbfe231a1d14671a2d87feb2
jdkfx <amateurranch@gmail.com>
2023-12-07 19:30:52 +0900

    :art: 経歴情報の欄にコンポーネントを追加

---
+++ src/lib/History.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '$lib/HistoryMiddle.svelte';
+	import HistoryContents from './HistoryContents.svelte';
+</script>
+
+<div>
+	<div class="my-10">
+		<h1 class="text-5xl font-bold">About Me</h1>
+	</div>
+
+	<div class="flex flex-col w-full border-opacity-50">
+		<div class="my-5 divider">
+			<h2 class="text-4xl text-center font-semibold">Career</h2>
+		</div>
+		<div>
+			<p class="text-2xl text-center font-semibold">経歴</p>
+		</div>
+		<div>
+			<HistoryContents />
+		</div>
+	</div>
+</div>
---
+++ src/lib/HistoryContents.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import HistoryMiddle from './HistoryMiddle.svelte';
+	import { histories } from '../routes/about/history-data';
+</script>
+
+<ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+	{#each { length: histories.length } as _, i}
+		{#if i % 2 === 0}
+			<li>
+				<hr />
+				<HistoryMiddle />
+				<div class="timeline-start md:text-end">
+					<time class="font-mono italic">{histories[i].date}</time>
+					<div class="text-lg font-black">{histories[i].company}</div>
+					<p>{histories[i].content}</p>
+				</div>
+				<hr />
+			</li>
+		{:else}
+			<li>
+				<hr />
+				<HistoryMiddle />
+				<div class="timeline-end">
+					<time class="font-mono italic">{histories[i].date}</time>
+					<div class="text-lg font-black">{histories[i].company}</div>
+					<p>{histories[i].content}</p>
+				</div>
+				<hr />
+			</li>
+		{/if}
+	{/each}
+</ul>
