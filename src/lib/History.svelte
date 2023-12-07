58874abe4b1c9fe031b805da5e6c79007cf7163f
jdkfx <amateurranch@gmail.com>
2023-12-07 21:24:07 +0900

    :art: History コンポーネントの修正

--- src/lib/History.svelte
+++ src/lib/History.svelte
@@ -3,11 +3,7 @@
 </script>
 
 <div>
-	<div class="my-10">
-		<h1 class="text-5xl font-bold">About Me</h1>
-	</div>
-
-	<div class="flex flex-col w-full border-opacity-50">
+	<div class="flex flex-col w-full border-opacity-50 my-10">
 		<div class="my-5 divider">
 			<h2 class="text-4xl text-center font-semibold">Career</h2>
 		</div>
